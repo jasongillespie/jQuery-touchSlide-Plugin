1637e9edeeb2a048ec0d821b2adb39c48feb009e
Jason <json.owen@gmail.com>
2013-03-11 09:56:20 -0400

    adding fallback arrows for non-touchscreen devices

--- coffeescript/jquery.touch-slide.js.coffee
+++ coffeescript/jquery.touch-slide.js.coffee
@@ -13,13 +13,20 @@ $ = jQuery
 pluginName = 'touchSlide'
 defaults = 
 	threshold: 10  	# percentage of element a finger needs to drag to change slides
-
+	next: '.next'	# click control for next slide
+	prev: '.prev'	# click control for previous slide
 
 ##
 #	SlideListener listens and responds to touch events
 #		enabling slide animations
 #
 #	@param jQuery object
+#	@param options properties to alter the default settings
+#		max_width - the maximum width of the window to call touch events
+#		prev - click control to view previous slide
+#		next - next control to view next slide
+#		onTouchStart - callback function on touchstart
+#		onTouchEnd - callback function on touchend
 #	@author Jason Gillespie
 ##
 class SlideListener
@@ -27,12 +34,6 @@ class SlideListener
 	constructor: (elem, options) ->
 
 		@elem = $(elem)
-
-		# Setup our listeners
-		@elem.on 'touchstart', @start
-		@elem.on 'touchmove', @move
-		@elem.on 'touchend', @end
-
 		@settings = $.extend {}, defaults, options
 
 		# Attach our state variables to the element
@@ -41,13 +42,76 @@ class SlideListener
 		@delta_x = 0
 		@delta_y = 0
 
-		@_defaults = defaults
 		@_name = pluginName
+		@_listening = false
+
+		# Listen for window width changes and listen/unlisten where appropriate
+		if @settings.max_width != undefined
+			@maxWidthListener()
+			$(window).resize =>
+				@maxWidthListener()
+		else
+			@listen()
+
+		@disableButton()
+
+	##
+	#  Does the device support the touch events?
+	##
+	isTouchEnabled: ->
+		return !!('ontouchstart' in window)
+
+	##
+	#	Bind/Unbind Events to the element per screen size
+	##
+	maxWidthListener: =>
+		if $(window).width() > @settings.max_width && @_listening
+			@unlisten()
+		else if $(window).width() <= @settings.max_width && !@_listening
+			@listen()
+
+
+	##
+	#  Bind touch events. Bind click events if device doesn't support touch methods
+	##
+	listen: =>
+
+		@_listening = true
+
+		if @isTouchEnabled()
+			@elem.on 'touchstart', @start
+			@elem.on 'touchmove', @move
+			@elem.on 'touchend', @end
+			$(@settings.next).on 'touchend', @next
+			$(@settings.prev).on 'touchend', @prev
+		else
+			$(@settings.next).on 'click', @next
+			$(@settings.prev).on 'click', @prev
+
+
+	##
+	#  Unbind all events
+	##
+	unlisten: =>
+		@_listening = false
+
+		if @isTouchEnabled()
+			@elem.unbind 'touchstart touchmove touchend'
+			$(@settings.next).unbind 'touchend', @next
+			$(@settings.prev).unbind 'touchend', @prev
+		else
+			$(@settings.next).unbind 'click', @next
+			$(@settings.prev).unbind 'click', @prev
+
 
 	###
 	#	Event fired on touchstart
 	###
 	start: (e) =>
+
+		# Fire Callback
+		if typeof @settings.onTouchSlideStart == 'function'
+			@settings.onTouchSlideStart.call this
 
 		touch = e.originalEvent.touches[0];
 
@@ -60,7 +124,6 @@ class SlideListener
 			'-moz-transition': '0'
 			'-webkit-transition': '0'
 			'transition': '0'
-
 
 
 	###
@@ -105,6 +168,10 @@ class SlideListener
 	###
 	end: (e) =>
 
+		# Fire Callback
+		if typeof @settings.onTouchSlideEnd == 'function'
+			@settings.onTouchSlideEnd.call this
+
 		# Enable transitions
 		@elem.children().css
 			'-moz-transition': ''
@@ -115,10 +182,10 @@ class SlideListener
 		min_threshold = Math.abs @delta_x / @elem.width() * 100
 
 		# Update active slide if the slide crosses the defined threshold 
-		if @delta_x < 0 && min_threshold > @settings.threshold && @elem.children('.active').next().length
-			@elem.children('.active').removeClass('active').next().addClass('active')
-		if @delta_x > 0 && min_threshold > @settings.threshold && @elem.children('.active').prev().length
-			@elem.children('.active').removeClass('active').prev().addClass('active')
+		if @delta_x < 0 && min_threshold > @settings.threshold
+			@next()
+		if @delta_x > 0 && min_threshold > @settings.threshold 
+			@prev()
 
 		if Math.abs(@delta_x) > 3
 			e.preventDefault()
@@ -126,6 +193,38 @@ class SlideListener
 		# reset delta
 		@delta_x = 0
 
+	###
+	# If there's a next slide, make it active
+	###
+	next: =>
+
+		if @elem.children('.active').next().length
+			@elem.children('.active').removeClass('active').next().addClass('active')
+
+		@disableButton()
+
+	###
+	# If there's a previous slide, make it active
+	###	
+	prev: =>
+
+		if @elem.children('.active').prev().length
+			@elem.children('.active').removeClass('active').prev().addClass('active')
+		
+		@disableButton()
+
+	###
+	# Disable buttons if they are useless
+	###
+	disableButton: =>
+		$(@settings.next).removeClass 'disabled'
+		$(@settings.prev).removeClass 'disabled'
+		
+		if @elem.children().last().hasClass 'active'
+			$(@settings.next).addClass 'disabled'
+
+		if @elem.children().first().hasClass 'active'
+			$(@settings.prev).addClass 'disabled'
 
 # Add object to jQuery
 $.fn.extend
--- jquery.touch-slide.js
+++ jquery.touch-slide.js
@@ -1,43 +1,107 @@
 (function() {
   var $, SlideListener, defaults, pluginName,
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   $ = jQuery;
 
   pluginName = 'touchSlide';
 
   defaults = {
-    threshold: 10
+    threshold: 10,
+    next: '.next',
+    prev: '.prev'
   };
 
   SlideListener = (function() {
 
     function SlideListener(elem, options) {
+      this.disableButton = __bind(this.disableButton, this);
+
+      this.prev = __bind(this.prev, this);
+
+      this.next = __bind(this.next, this);
+
       this.end = __bind(this.end, this);
 
       this.move = __bind(this.move, this);
 
       this.start = __bind(this.start, this);
+
+      this.unlisten = __bind(this.unlisten, this);
+
+      this.listen = __bind(this.listen, this);
+
+      this.maxWidthListener = __bind(this.maxWidthListener, this);
+
+      var _this = this;
       this.elem = $(elem);
-      this.elem.on('touchstart', this.start);
-      this.elem.on('touchmove', this.move);
-      this.elem.on('touchend', this.end);
       this.settings = $.extend({}, defaults, options);
       this.start_x = 0;
       this.start_y = 0;
       this.delta_x = 0;
       this.delta_y = 0;
-      this._defaults = defaults;
       this._name = pluginName;
+      this._listening = false;
+      if (this.settings.max_width !== void 0) {
+        this.maxWidthListener();
+        $(window).resize(function() {
+          return _this.maxWidthListener();
+        });
+      } else {
+        this.listen();
+      }
+      this.disableButton();
     }
 
+    SlideListener.prototype.isTouchEnabled = function() {
+      return !!(__indexOf.call(window, 'ontouchstart') >= 0);
+    };
+
+    SlideListener.prototype.maxWidthListener = function() {
+      if ($(window).width() > this.settings.max_width && this._listening) {
+        return this.unlisten();
+      } else if ($(window).width() <= this.settings.max_width && !this._listening) {
+        return this.listen();
+      }
+    };
+
+    SlideListener.prototype.listen = function() {
+      this._listening = true;
+      if (this.isTouchEnabled()) {
+        this.elem.on('touchstart', this.start);
+        this.elem.on('touchmove', this.move);
+        this.elem.on('touchend', this.end);
+        $(this.settings.next).on('touchend', this.next);
+        return $(this.settings.prev).on('touchend', this.prev);
+      } else {
+        $(this.settings.next).on('click', this.next);
+        return $(this.settings.prev).on('click', this.prev);
+      }
+    };
+
+    SlideListener.prototype.unlisten = function() {
+      this._listening = false;
+      if (this.isTouchEnabled()) {
+        this.elem.unbind('touchstart touchmove touchend');
+        $(this.settings.next).unbind('touchend', this.next);
+        return $(this.settings.prev).unbind('touchend', this.prev);
+      } else {
+        $(this.settings.next).unbind('click', this.next);
+        return $(this.settings.prev).unbind('click', this.prev);
+      }
+    };
+
     /*
-      # Event fired on touchstart
+    	#	Event fired on touchstart
     */
 
 
     SlideListener.prototype.start = function(e) {
       var touch;
+      if (typeof this.settings.onTouchSlideStart === 'function') {
+        this.settings.onTouchSlideStart.call(this);
+      }
       touch = e.originalEvent.touches[0];
       this.start_x = touch.pageX;
       this.start_y = touch.pageY;
@@ -49,7 +113,7 @@
     };
 
     /*
-      # Event fired on touchmove
+    	# Event fired on touchmove
     */
 
 
@@ -78,12 +142,15 @@
     };
 
     /*
-      # Event fired on touchend
+    	# Event fired on touchend
     */
 
 
     SlideListener.prototype.end = function(e) {
       var min_threshold;
+      if (typeof this.settings.onTouchSlideEnd === 'function') {
+        this.settings.onTouchSlideEnd.call(this);
+      }
       this.elem.children().css({
         '-moz-transition': '',
         '-webkit-transition': '',
@@ -91,16 +158,56 @@
         'left': ''
       });
       min_threshold = Math.abs(this.delta_x / this.elem.width() * 100);
-      if (this.delta_x < 0 && min_threshold > this.settings.threshold && this.elem.children('.active').next().length) {
-        this.elem.children('.active').removeClass('active').next().addClass('active');
+      if (this.delta_x < 0 && min_threshold > this.settings.threshold) {
+        this.next();
       }
-      if (this.delta_x > 0 && min_threshold > this.settings.threshold && this.elem.children('.active').prev().length) {
-        this.elem.children('.active').removeClass('active').prev().addClass('active');
+      if (this.delta_x > 0 && min_threshold > this.settings.threshold) {
+        this.prev();
       }
       if (Math.abs(this.delta_x) > 3) {
         e.preventDefault();
       }
       return this.delta_x = 0;
+    };
+
+    /*
+    	# If there's a next slide, make it active
+    */
+
+
+    SlideListener.prototype.next = function() {
+      if (this.elem.children('.active').next().length) {
+        this.elem.children('.active').removeClass('active').next().addClass('active');
+      }
+      return this.disableButton();
+    };
+
+    /*
+    	# If there's a previous slide, make it active
+    */
+
+
+    SlideListener.prototype.prev = function() {
+      if (this.elem.children('.active').prev().length) {
+        this.elem.children('.active').removeClass('active').prev().addClass('active');
+      }
+      return this.disableButton();
+    };
+
+    /*
+    	# Disable buttons if they are useless
+    */
+
+
+    SlideListener.prototype.disableButton = function() {
+      $(this.settings.next).removeClass('disabled');
+      $(this.settings.prev).removeClass('disabled');
+      if (this.elem.children().last().hasClass('active')) {
+        $(this.settings.next).addClass('disabled');
+      }
+      if (this.elem.children().first().hasClass('active')) {
+        return $(this.settings.prev).addClass('disabled');
+      }
     };
 
     return SlideListener;
